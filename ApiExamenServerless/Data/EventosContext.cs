695f0af3a13c06e823a0096e4481db890af35f4e
Miguelrc9991 <miguel.ricocorrales@gmail.com>
2022-05-11 13:01:50 +0200

    Add project files.

---
+++ ApiExamenServerless/Data/EventosContext.cs
@@ -0,0 +1,14 @@
+﻿using ApiExamenMR.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiExamenMR.Data {
+    public class EventosContext: DbContext {
+        public EventosContext(DbContextOptions<EventosContext> options): base(options) { }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Evento> Eventos { get; set; }
+    }
+}
